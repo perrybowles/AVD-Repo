16f304a9586c8bff67efb4647df88542c1d55012
Perry <75803259+perrybowles@users.noreply.github.com>
2021-09-28 16:57:42 +0100

    Add files via upload

--- LangPack.ps1
+++ LangPack.ps1
@@ -21,27 +21,28 @@ Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFe
 Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-Speech-en-gb-Package~31bf3856ad364e35~amd64~~.cab
 Add-WindowsPackage -Online -PackagePath $LIPContent\Microsoft-Windows-LanguageFeatures-TextToSpeech-en-gb-Package~31bf3856ad364e35~amd64~~.cab
 
-Start-Sleep 10
 # Set UK timezone
 Set-TimeZone "GMT Standard Time"
 
 # Set UK locale, language etc. for current user
 New-WinUserLanguageList -Language en-GB
 Set-WinUILanguageOverride -Language en-GB
-Set-Culture en-GB
+Set-WinDefaultInputMethodOverride -InputTip "0809:00000809"
 Set-WinSystemLocale -SystemLocale en-GB
 Set-WinUserLanguageList -LanguageList en-GB -Force
 Set-WinHomeLocation -GeoId 242
-Set-WinDefaultInputMethodOverride -InputTip "0809:00000809"
+Set-Culture en-GB
 
 #Copy current user's region settings to System & Default users + Welcome screen
+Start-Sleep 5
 & $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"F:\Scripts\en-GB\CopyRegion.xml`""
+
+#Delete drive mapping and D:\LangPack folder
+#net use f: /delete /y
+remove-item -path d:\LangPack -recurse
 
 #Set the execution policy to default for current user
 Set-ExecutionPolicy -ExecutionPolicy default -scope currentuser -Force
-
-#Delete drive mapping
-net use f: /delete /y
 
 #Reboot immediately
 shutdown /r /t 0
